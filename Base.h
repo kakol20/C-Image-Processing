58ae7448844bfa3541a2c85c913d6d595d2189af
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-01-20 21:26:07 +0000

    Initial delete for version 2

--- Base.h
+++
@@ -1,9 +0,0 @@
-#ifndef BASE_H
-#define BASE_H
-
-enum class ImageType
-{
-	PNG, JPG, BMP, TGA
-};
-
-#endif // !BASE_H
--- Functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-#include "Image.h"
-#include "String.h"
-
-void RollingShutter();
-
-#endif // !FUNCTIONS_H
