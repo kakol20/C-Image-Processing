4b5d30411b2c2072c646b2ad666db3a102223a90
Adrian Aquino-Neary <adrianaquino.aquino33@gmail.com>
2023-02-03 16:50:12 +0000

    Simplify cubic formula

--- src/maths/Maths.cpp
+++ src/maths/Maths.cpp
@@ -1,5 +1,23 @@
 #include "Maths.h"
 
 float Maths::Cubic(const float& a, const float& b, const float& c, const float& d, const float factor) {
-	return b + 0.5f * factor * (c - a + factor * (2.f * a - 5.f * b + 4.f * c - d + factor * (3.f * (b - c) + d - a)));
+	//return b + 0.5f * factor * (c - a + factor * (2.f * a - 5.f * b + 4.f * c - d + factor * (3.f * (b - c) + d - a)));
+
+	const float factor2 = Maths::LoopPow(factor, 2);
+	const float factor3 = factor2 * factor;
+
+	return b + (0.5f * factor * c) - (0.5f * factor * a) + (factor2 * a) -
+		(2.5f * b * factor2) + (2.f * factor2 * c) - (0.5f * factor2 * d) +
+		(1.5f * b * factor3) - (1.5f * factor3 * c) + (0.5f * factor3 * d) - (0.5f * factor3 * a);
+}
+
+float Maths::LoopPow(const float& n, const unsigned int& p) {
+	if (p == 0) return 1.f;
+	if (p == 1) return n;
+
+	float r = n;
+	for (unsigned int i = 0; i < p - 1; i++) {
+		r *= n;
+	}
+	return r;
 }
--- src/maths/Maths.h
+++ src/maths/Maths.h
@@ -5,4 +5,6 @@
 
 namespace Maths {
 	float Cubic(const float& a, const float& b, const float& c, const float& d, const float factor);
+
+	float LoopPow(const float& n, const unsigned int& p);
 }
